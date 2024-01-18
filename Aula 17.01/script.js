afdb2f4bf5098c6edc8d03e0ba7df29a51843205
Douglas Rosa <144066281+DoughRosa@users.noreply.github.com>
2024-01-18 14:38:18 -0300

    Add files via upload

---
+++ Aula 17.01/script.js
@@ -0,0 +1,90 @@
+// Crie uma função para cada item que peça para pessoa usuária um número e imprima de acordo com os tópicos :
+
+// a-) O numero pedido
+
+// function mostrar() {
+//     let numero = Number(prompt("Digite um número:"));
+//     console.log(`O número pedido é:  ${numero}`);
+// }
+
+// mostrar();
+
+// // b-) Os numeros pares maiores que 10
+
+// let armazenamento = [];
+// let numero = Number(prompt("Digite um número:"));
+
+// if(numero>10 && numero%2 == 0){
+//     armazenamento.push(numero);
+// };
+
+// function maioresQuedez(){
+//     console.log(numero)
+// };
+
+// maioresQuedez();
+
+// // c-) Todos os números de zero ao número passado
+
+// let armazenamento = [];
+// let numero = Number(prompt("Digite um número:"));
+
+// for(let i=0; i<=numero; i++){
+//     armazenamento.push(i);
+// };
+
+// function todosAtenumero(){
+//     document.write(armazenamento);
+// };
+
+// todosAtenumero();
+
+// // d-) Insira 5 vezes em uma lista os 5 numeros passados
+
+// let armazenamento = [];
+
+// for(let i=0; i<=4; i++){
+//     let numero = Number(prompt("Digite um número:"));
+//     armazenamento.push(numero);
+//     armazenamento.push(numero);
+//     armazenamento.push(numero);
+//     armazenamento.push(numero);
+//     armazenamento.push(numero);
+// };
+
+// function exercicioD(){
+//     document.write(armazenamento);
+// };
+
+// exercicioD();
+
+// e-) armazena na lista 5 números e retorne o número cujo o indice é 3
+
+// let armazenamento = [];
+
+// for(let i=0; i<=4; i++){
+//     let numero = Number(prompt("Digite um número:"));
+//     armazenamento.push(numero);
+// };
+
+// function terceiraPosicao(){
+//    document.write(armazenamento[2]);
+// };
+
+// terceiraPosicao();
+
+// // f-) Armazene uma lista de 5 números e multiplique todos os números por 3
+
+// let armazenamento = [];
+
+// for(let i=0; i<=4; i++){
+//     let numero = Number(prompt("Digite um número:"));
+//     armazenamento.push(numero);
+// };
+
+// function multiplicarPortres(){
+//     let vezesTres = armazenamento.map(numero => numero*3);
+//     document.write(vezesTres);
+// };
+
+// multiplicarPortres();
